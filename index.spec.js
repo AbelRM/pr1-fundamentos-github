35a03dabe24bc8586ac107c10da07631bde97f7c
Abel Maquera <jhanmaquera@gmail.com>
2025-08-06 22:50:26 -0500

    Corregido index spec js

--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,7 @@
 const dice = require("./index");
 test("Retorna un numero random de 1 al 6", () => {
   const rollResult = dice.roll();
-  console.log("resultado de ${rollResult}");
+  console.log(`result was ${rollResult}`);
   expect(rollResult).toBeGreaterThanOrEqual(1);
   expect(rollResult).toBeLessThanOrEqual(6);
 })
